19c8eb57907c1939b6ee631ea818884b43e4ef00
Sebas <Qsebas56@gmail.com>
2024-06-19 15:41:02 -0500

    2.0

--- src/udla/aquishpe/arboles/maxProfundidad.java
+++ src/udla/aquishpe/arboles/maxProfundidad.java
@@ -1,4 +1,26 @@
 package udla.aquishpe.arboles;
 
+
+
+/*
+                4
+        2                 7
+     1     3
+  9
+
+*/
 public class maxProfundidad {
+
+    public int mP(Node root){
+
+        if(root==null) return 0;
+
+        int dletf=mP(root.left)+1;   /// el nodo raiz cuenta como nivel 0
+        int dright=mP(root.right)+1;
+
+        return Math.max(dletf,dright);
+
+
+    }
+
 }
